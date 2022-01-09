9e387aee04f6d782b5d108fb61b7e444ad5245b4
João Bispo <joaobispo2077@gmail.com>
2022-01-09 11:09:05 -0300

    feat: add CreateClientController

---
+++ src/domain/useCases/createClient/CreateClientController.ts
@@ -0,0 +1,18 @@
+import { RequestHandler } from 'express';
+
+import { prisma } from '../../../infra/database/prismaClient';
+import { CreateClientUseCase } from './CreateClientUseCase';
+
+export class CreateClientController {
+  handle: RequestHandler = async (request, response) => {
+    const { username, password } = request.body;
+
+    const createClientUseCase = new CreateClientUseCase(prisma);
+    const createdClient = await createClientUseCase.execute({
+      username,
+      password,
+    });
+
+    return response.json(createdClient);
+  };
+}
